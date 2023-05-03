947416edb27ae7b91c2bf27583b499e585de5454
swilliamsilva <williamsilva.codigo@gmail.com>
2023-05-03 15:16:31 -0300

    Implementado os botões limpar e sair

---
+++ Resultado.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoTripleX
+{
+    public class Resultado
+    {
+
+        [JsonProperty(PropertyName = "cep")]
+        public string CEP { get; set; }
+
+        [JsonProperty(PropertyName = "logradouro")]
+        public string Logradouro { get; set; }
+
+        [JsonProperty(PropertyName = "complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty(PropertyName = "bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty(PropertyName = "localidade")]
+        public string Localidade { get; set; }
+
+        [JsonProperty(PropertyName = "uf")]
+        public string Uf { get; set; }
+
+        [JsonProperty(PropertyName = "ibge")]
+        public string Ibge { get; set; }
+
+        [JsonProperty(PropertyName = "gia")]
+        public string Gia { get; set; }
+
+        [JsonProperty(PropertyName = "DDD")]
+        public string DDD { get; set; }
+
+        [JsonProperty(PropertyName = "siafi")]
+        public string Siafi { get; set; }
+
+
+
+
+
+
+    }
+}
